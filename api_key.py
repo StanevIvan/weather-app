b17f7fc2f0eb378780507e167fc9c125a7e50cfd
Ivan Stanev <baiivan.stanev@gmail.com>
2023-09-11 15:32:08 +0300

    Update via upload

---
+++ api_key.py
@@ -0,0 +1,32 @@
+# API key generator and saver
+#TODO Make the API key generator to save the data to a DB, so that it can be used later.
+#TODO Make the API key generator to check if the key is already in the DB, so that it can be used later.
+#TODO Make the API key generator to check if the key is valid.
+#TODO Make the API key generator to check if the key is expired.
+#TODO Make the API key generator to check if the key is used.
+#TODO Make the API key generator to check if the key is used by the same user.
+#TODO Make the API key generator throw an error if the user tries to make a new key before the old one is expired.
+
+import secrets
+import string
+
+
+class ApiKey:
+    def __init__(self):
+        self.key = self.key_gen()
+
+    def key_gen(self):
+        alphabet = string.ascii_letters + string.digits
+        key = ''.join(secrets.choice(alphabet) for i in range(32))
+        return key
+
+    def save_key(self):
+        with open("api_key.txt", "w") as file:
+            file.write(self.key)
+
+    def get_key(self):
+        with open("api_key.txt", "r") as file:
+            key = file.read()
+        return key
+
+
